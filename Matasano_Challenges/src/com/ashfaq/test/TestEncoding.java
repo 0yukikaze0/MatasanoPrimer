84787d24720bf05d501b285de84ed794298572b3
Ashfaq <neothedefender@gmail.com>
2016-05-05 00:17:55 +0530

    Added XOR cipher and binaryToHex decoder

--- Matasano_Challenges/src/com/ashfaq/test/TestEncoding.java
+++ Matasano_Challenges/src/com/ashfaq/test/TestEncoding.java
@@ -21,6 +21,7 @@ public class TestEncoding {
 		String hexString = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
 		String hexBase64 = encoder.hexToBase64(hexString);
 		/*---------------------------------------------------------*/
+		
 	}
 	
 }
